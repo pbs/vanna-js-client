c68e41e342c9560e201c2565b3eefd9141cb371e
Yanglin Zhao <yanglinz@users.noreply.github.com>
2018-06-28 10:01:47 -0400

    Change value -> enabled (#5)

--- src/vanna.test.js
+++ src/vanna.test.js
@@ -1,16 +1,17 @@
-import { VannaClient } from "./vanna";
+import { VannaClient, isFeatureEnabled } from "./vanna";
 
 import examplePayload from "../data/example.json";
 
-describe("vanna", () => {
+describe("vanna client interface", () => {
   const getPayload = () => Promise.resolve(examplePayload);
 
   const server = "https://vanna.example.com/";
   const project = "example-project-name";
 
   it("should have valid example payload", () => {
-    expect(examplePayload.features["some-feature"].value).toEqual(true);
-    expect(examplePayload.features["another-feature"].value).toEqual(true);
+    expect(examplePayload.features["some-feature"].enabled).toEqual(true);
+    expect(examplePayload.features["another-feature"].enabled).toEqual(true);
+    expect(examplePayload.features["disabled-feature"].enabled).toEqual(false);
   });
 
   it("should setup client and get values", cb => {
@@ -26,6 +27,36 @@ describe("vanna", () => {
       expect(anotherFeature).toEqual(true);
 
       cb();
+    });
+  });
+});
+
+describe("vanna helpers", () => {
+  describe("isFeatureEnabled helper", () => {
+    it("should handle user segment matching", () => {
+      const feature = {
+        type: "boolean",
+        enabled: true,
+        targetSegment: ["some-segment"]
+      };
+      const userSegment = "some-segment";
+
+      const actual = isFeatureEnabled(feature, { userSegment });
+      const expected = true;
+      expect(actual).toEqual(expected);
+    });
+
+    it("should handle user segment not matching", () => {
+      const feature = {
+        type: "boolean",
+        enabled: true,
+        targetSegment: ["some-segment"]
+      };
+      const userSegment = "another-segment";
+
+      const actual = isFeatureEnabled(feature, { userSegment });
+      const expected = false;
+      expect(actual).toEqual(expected);
     });
   });
 });

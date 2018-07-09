33642a4273b068c03474ca5da5bfb42abdea23a8
Yanglin Zhao <yanglinz@users.noreply.github.com>
2018-07-09 13:32:08 -0400

    Tweak setup and override api (#9)

--- example/index.js
+++ example/index.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+import { VannaClient } from "../src/vanna";
+
+import exampleManifest from "../data/example.json";
+
+const client = new VannaClient({
+  uri: "https://vanna.example.com/project-manifest",
+  userSegment: "alpha-users",
+  _overrides: {
+    getManifest: () => Promise.resolve(exampleManifest)
+  }
+});
+
+client.on("ready", () => {
+  const isSomeFeatureEnabled = client.variation("some-feature", {
+    fallback: false
+  });
+  if (isSomeFeatureEnabled) {
+    console.log("Some feature is enabled :)");
+  } else {
+    console.log("Some feature is disabled :(");
+  }
+});
--- src/vanna.test.js
+++ src/vanna.test.js
@@ -1,12 +1,12 @@
-import { VannaClient, isFeatureEnabled } from "./vanna";
+import { VannaClient, getFeatureVariation } from "./vanna";
 
 import examplePayload from "../data/example.json";
 
 describe("vanna client interface", () => {
-  const getPayload = () => Promise.resolve(examplePayload);
-
-  const server = "https://vanna.example.com/";
-  const project = "example-project-name";
+  const client = new VannaClient({
+    uri: "https://vanna.example.com/project",
+    _overrides: { getManifest: () => Promise.resolve(examplePayload) }
+  });
 
   it("should have valid example payload", () => {
     expect(examplePayload.features["some-feature"].enabled).toEqual(true);
@@ -15,9 +15,6 @@ describe("vanna client interface", () => {
   });
 
   it("should setup client and get values", cb => {
-    const vanna = new VannaClient({ getPayload });
-    const client = vanna.setup({ server, project });
-
     client.on("ready", () => {
       const someFeature = client.variation("some-feature", { fallback: true });
       const anotherFeature = client.variation("another-feature", {
@@ -32,7 +29,7 @@ describe("vanna client interface", () => {
 });
 
 describe("vanna helpers", () => {
-  describe("isFeatureEnabled helper", () => {
+  describe("getFeatureVariation helper", () => {
     it("should handle user segment matching", () => {
       const feature = {
         type: "boolean",
@@ -41,7 +38,7 @@ describe("vanna helpers", () => {
       };
       const userSegment = "some-segment";
 
-      const actual = isFeatureEnabled(feature, { userSegment });
+      const actual = getFeatureVariation(feature, { userSegment });
       const expected = true;
       expect(actual).toEqual(expected);
     });
@@ -54,7 +51,7 @@ describe("vanna helpers", () => {
       };
       const userSegment = "another-segment";
 
-      const actual = isFeatureEnabled(feature, { userSegment });
+      const actual = getFeatureVariation(feature, { userSegment });
       const expected = false;
       expect(actual).toEqual(expected);
     });

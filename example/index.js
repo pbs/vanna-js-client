cc73972fc8e4808571eb5a942a17ddae849f0df2
Yanglin Zhao <yz1876@gmail.com>
2018-07-24 14:19:28 -0400

    Allow fallbacks to be set globally

--- example/index.js
+++ example/index.js
@@ -5,15 +5,16 @@ import exampleManifest from "../data/example.json";
 const client = new VannaClient({
   uri: "https://vanna.example.com/project-manifest",
   userSegment: "alpha-users",
+  fallbacks: {
+    "some-feature": false
+  },
   _overrides: {
     getManifest: () => Promise.resolve(exampleManifest)
   }
 });
 
 client.on("ready", () => {
-  const isSomeFeatureEnabled = client.variation("some-feature", {
-    fallback: false
-  });
+  const isSomeFeatureEnabled = client.variation("some-feature");
   if (isSomeFeatureEnabled) {
     console.log("Some feature is enabled :)");
   } else {

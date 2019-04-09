337c68b38dc02c5e483b0b3f7e9e1c2ae5c0369c
Yanglin Zhao <yz1876@gmail.com>
2019-04-09 11:33:22 -0400

    Add tests with multiple sources

--- src/tests/integration.test.ts
+++ src/tests/integration.test.ts
@@ -15,8 +15,7 @@ describe("synchronous api", () => {
         ]
       });
 
-      const isEnabled = client.variation("some-feature");
-      expect(isEnabled).toEqual(true);
+      expect(client.variation("some-feature")).toEqual(true);
     });
 
     it("should throw an error if feature can't match target", () => {
@@ -51,8 +50,7 @@ describe("synchronous api", () => {
         ]
       });
 
-      const isEnabled = client.variation("some-feature");
-      expect(isEnabled).toEqual(true);
+      expect(client.variation("some-feature")).toEqual(true);
     });
 
     it("should handle when a target doesn't match", () => {
@@ -70,8 +68,7 @@ describe("synchronous api", () => {
         ]
       });
 
-      const isEnabled = client.variation("some-feature");
-      expect(isEnabled).toEqual(false);
+      expect(client.variation("some-feature")).toEqual(false);
     });
 
     it("should handle featureName mismatch", () => {
@@ -91,11 +88,132 @@ describe("synchronous api", () => {
     });
   });
 
-  it("should handle multiple sources", () => {
-    // todo
-  });
+  describe("multiple target", () => {
+    it("should handle features without targets", () => {
+      const client = new FeatureClient({
+        sources: [
+          source(() => [
+            {
+              type: "boolean",
+              slug: "some-feature",
+              enabled: true
+            }
+          ]),
+          source(() => [
+            {
+              type: "boolean",
+              slug: "another-feature",
+              enabled: true
+            }
+          ])
+        ]
+      });
 
-  it("should handle multiple sources of the same  feature", () => {
-    // todo
+      expect(client.variation("some-feature")).toEqual(true);
+      expect(client.variation("another-feature")).toEqual(true);
+    });
+
+    it("should throw an error if feature can't match target", () => {
+      const client = new FeatureClient({
+        sources: [
+          source(() => [
+            {
+              type: "boolean",
+              slug: "some-feature",
+              enabled: true,
+              targets: ["alpha-user"]
+            }
+          ]),
+          source(() => [
+            {
+              type: "boolean",
+              slug: "another-feature",
+              enabled: true,
+              targets: ["beta-user"]
+            }
+          ])
+        ]
+      });
+
+      expect(() => client.variation("some-feature")).toThrow();
+      expect(() => client.variation("another-feature")).toThrow();
+    });
+
+    it("should handle when a target matches", () => {
+      const client = new FeatureClient({
+        target: "alpha-user",
+        sources: [
+          source(() => [
+            {
+              type: "boolean",
+              slug: "some-feature",
+              enabled: true,
+              targets: ["alpha-user"]
+            }
+          ]),
+          source(() => [
+            {
+              type: "boolean",
+              slug: "another-feature",
+              enabled: true,
+              targets: ["alpha-user"]
+            }
+          ])
+        ]
+      });
+
+      expect(client.variation("some-feature")).toEqual(true);
+      expect(client.variation("another-feature")).toEqual(true);
+    });
+
+    it("should handle when a target doesn't match", () => {
+      const client = new FeatureClient({
+        target: "alpha-user",
+        sources: [
+          source(() => [
+            {
+              type: "boolean",
+              slug: "some-feature",
+              enabled: true,
+              targets: ["beta-user"]
+            }
+          ]),
+          source(() => [
+            {
+              type: "boolean",
+              slug: "another-feature",
+              enabled: true,
+              targets: ["beta-user"]
+            }
+          ])
+        ]
+      });
+
+      expect(client.variation("some-feature")).toEqual(false);
+      expect(client.variation("another-feature")).toEqual(false);
+    });
+
+    it("should handle featureName mismatch", () => {
+      const client = new FeatureClient({
+        sources: [
+          source(() => [
+            {
+              type: "boolean",
+              slug: "some-feature",
+              enabled: true
+            }
+          ]),
+          source(() => [
+            {
+              type: "boolean",
+              slug: "another-feature",
+              enabled: true
+            }
+          ])
+        ]
+      });
+
+      expect(() => client.variation("wrong-feature")).toThrow();
+    });
   });
 });
